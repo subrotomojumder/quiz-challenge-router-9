0b39319641545c77b616b333b1987fca0106775f
subrotomojumder <97802263+subrotomojumder@users.noreply.github.com>
2022-10-11 17:25:34 +0600

    assingment main work complete

--- src/component/Question.js
+++ src/component/Question.js
@@ -1,28 +1,42 @@
-import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/solid';
+import { EyeIcon, EyeSlashIcon, CheckIcon} from '@heroicons/react/24/solid';
 import React from 'react';
 import { useState } from 'react';
+import { toast } from 'react-toastify';
 
 const Question = ({ questionCon, indx }) => {
     const [isOpen, setIsOpen] = useState(false);
-    const number = ++indx;
     const { options, question, correctAnswer, id } = questionCon;
-    // console.log(question)
+
     const handleAnsShow = () => {
-        console.log(isOpen)
+        !isOpen && toast.warning(`${correctAnswer}`, {autoClose: 1500})
         setIsOpen(!isOpen)
     }
+    setTimeout(()=> setIsOpen(false) , 2500);
+    
+    const handleMcqAnswer = (ans) => {
+        if (ans === correctAnswer) {
+            toast.success('wow! Your answer correct...')
+        }
+        else{
+            toast.error('wrong! your answer wrong...')
+        }
+    }
+
     return (
         <div className='bg-blue-300 shadow-lg shadow-neutral-200 border rounded-md p-4 w-1/2 mx-auto my-5'>
-            <div onClick={handleAnsShow} className='w-6 ml-auto'>
+            <div onClick={handleAnsShow} className='w-8 ml-auto bg-orange-200 rounded-full p-1'>
                 {
-                    isOpen ? <EyeIcon></EyeIcon> :<EyeSlashIcon></EyeSlashIcon>
+                    isOpen ? <EyeIcon></EyeIcon> : <EyeSlashIcon></EyeSlashIcon>
                 }
             </div>
-            <h3 className='text-center mb-4'><span className='font-bold'>Quiz {number}: </span> <br /> <span className='text-lg font-semibold'>{question}</span></h3>
+            <h3 className='text-center mb-4'><span className='font-bold'>Quiz {++indx}: </span> <br /> <span className='text-lg font-semibold'>{question}</span></h3>
             <div className='grid grid-cols-2'>
                 {
-                    options.map((option, inx) => <div className='m-4 p-4 rounded-md bg-orange-100 hover:bg-orange-300 border-2 border-white' key={inx}>
-                        <p className='text-semibold'>{option}</p>
+                    options.map((option, i) => <div className='m-4 rounded-md bg-orange-100 hover:bg-orange-300 border-2 border-white' key={i}>
+                        <div onClick={()=>handleMcqAnswer(option)} className='flex items-center font-semibold'>
+                            <p className=' ml-4'><CheckIcon className='text-orange-500 w-6' /> </p>
+                            <p className='ml-3 py-4'>{option}</p>
+                        </div>
                     </div>)
                 }
             </div>
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from 'react-toastify';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <ToastContainer position='top-center' autoClose='1000' />
   </React.StrictMode>
 );
 
