6c00f1edab1cf0e11c285f39f29dfd8705ab7b19
subrotomojumder <97802263+subrotomojumder@users.noreply.github.com>
2022-10-11 15:17:55 +0600

    eye slash conditionaly change

--- src/component/Question.js
+++ src/component/Question.js
@@ -1,18 +1,29 @@
+import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/solid';
 import React from 'react';
+import { useState } from 'react';
 
 const Question = ({ questionCon, indx }) => {
+    const [isOpen, setIsOpen] = useState(false);
     const number = ++indx;
-    // console.log(questionCon)
     const { options, question, correctAnswer, id } = questionCon;
     // console.log(question)
+    const handleAnsShow = () => {
+        console.log(isOpen)
+        setIsOpen(!isOpen)
+    }
     return (
         <div className='bg-blue-300 shadow-lg shadow-neutral-200 border rounded-md p-4 w-1/2 mx-auto my-5'>
-            <h3 className='text-center'><span className='font-bold'>Quiz {number}: </span> <br /> <span className='text-lg font-semibold'>{question}</span></h3>
+            <div onClick={handleAnsShow} className='w-6 ml-auto'>
+                {
+                    isOpen ? <EyeIcon></EyeIcon> :<EyeSlashIcon></EyeSlashIcon>
+                }
+            </div>
+            <h3 className='text-center mb-4'><span className='font-bold'>Quiz {number}: </span> <br /> <span className='text-lg font-semibold'>{question}</span></h3>
             <div className='grid grid-cols-2'>
                 {
-                    options.map(option => <div className='border-2 m-4 p-4 rounded-md'>
-                         <p className='text-semibold'>{option}</p>
-                         </div>)
+                    options.map((option, inx) => <div className='m-4 p-4 rounded-md bg-orange-100 hover:bg-orange-300 border-2 border-white' key={inx}>
+                        <p className='text-semibold'>{option}</p>
+                    </div>)
                 }
             </div>
         </div>
