3c0824e86e4be723a74ae521846e070c8a40d270
subrotomojumder <97802263+subrotomojumder@users.noreply.github.com>
2022-10-11 21:35:01 +0600

    routing purpose add by blog

--- src/component/Blog.js
+++ src/component/Blog.js
@@ -2,8 +2,21 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
-            <h1>blog container</h1>
+        <div className='px-[15%] bg-cyan-300'>
+            <h1 className='text-center text-green-500 text-3xl pt-8 pb-2'>Question & Answer</h1>
+            <hr className='md:mx-[20%] bg-red-400 h-1 rounded-md mb-6' />
+            <div>
+                <p className='text-xl font-medium mb-2'><span className='font-bold'>Q.1. </span>What is the purpose of React Router ?</p>
+                <p>
+                    <span className='font-bold text-orange-300 underline ml-4'>Ans: </span>
+                    React Router হল React Routing করার জন্য একটি standard library । এটি একটি React application এর বিভিন্ন উপাদানের ভিউগুলির মধ্যে
+                     Navigation সক্ষম করে, Browser URL change করতে দেয় এবং UI-কে URL-এর সাথে adjusting রাখে । React Router হল declaretive এ জন্য 
+                     React Router use করে user application এর কোন component সেটা নির্দিষ্ট করে application কে users friendly এবং easier করে তোলা যায় ।
+                     হল । এর মানে হলো library থেকে সব কিছু read করার পরিবর্তে শুধু প্রয়োজনীয় feature গুলোকে use করা যায় । React Router হল asynchronous 
+                     এজন্য applications component গুলো সব একসাথে load না হয়ে user এর চাহিদা অনুযায়ি load হয় । এতে user experience ভালো থাকে ।
+                     একাধিক Root একত্রিত করে complex Root তৈরি করা যায় এতে Routing flexible হয় ।
+                </p>
+            </div>
         </div>
     );
 };
